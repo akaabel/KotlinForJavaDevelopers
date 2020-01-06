a00957a6957182826beea8d7289d3aeaa4029881
Alf-Kenneth Aabel <aka@decisive.no>
2020-01-06 18:46:24 +0100

    Ferdig med Section 5 challenge 1.

--- src/academy/learnprogramming/oochallenge/KotlinBicycle.kt
+++ src/academy/learnprogramming/oochallenge/KotlinBicycle.kt
@@ -8,16 +8,31 @@ fun main() {
     b.speedUp(20)
     println("Speed: ${b.speed}")
 
+    b.printDescription()
+
     val m = KotlinMountainBike(10, 20, 30, 40)
     println("Seatheight ${m.seatHeight}")
+    m.printDescription()
 
     val r = RoadBike(10, 20, 30, 40)
     println("TireWidth ${r.tireWidth}")
+    r.printDescription()
 }
 
-class RoadBike(cadence: Int, speed: Int, gear: Int, val tireWidth: Int) : KotlinBicycle(cadence, speed, gear)
+class RoadBike(cadence: Int, speed: Int, gear: Int, val tireWidth: Int) : KotlinBicycle(cadence, speed, gear) {
+    override fun printDescription() {
+        super.printDescription()
+        println("The roadbike has tirewidth $tireWidth")
+    }
+}
 
-class KotlinMountainBike(var seatHeight: Int, cadence: Int, speed: Int, gear: Int) : KotlinBicycle(cadence, speed, gear)
+class KotlinMountainBike(var seatHeight: Int, cadence: Int, speed: Int, gear: Int) : KotlinBicycle(cadence, speed, gear) {
+    override fun printDescription() {
+        super.printDescription()
+        println("The mountainbike has seatheigth $seatHeight")
+    }
+
+}
 
 open class KotlinBicycle( var cadence: Int, var speed: Int, var gear: Int) {
     fun applyBrake(decrement: Int) {
@@ -26,5 +41,9 @@ open class KotlinBicycle( var cadence: Int, var speed: Int, var gear: Int) {
 
     fun speedUp(increment: Int) {
         speed += increment
+    }
+
+    open fun printDescription() {
+        println("Bike is in gear $gear with a cadence of $cadence travelling at a speed of $speed")
     }
 }
